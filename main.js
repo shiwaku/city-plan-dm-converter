0097d4ba4e5c5467966989c785cfaf31aa63aec3
shiwaku <yohei.shiwaku@gmail.com>
2026-03-08 13:50:16 +0900

    minZoomを12に制限

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const map = new maplibregl.Map({
   container: "map",
   style: "std.json",
   zoom: 15.8,
-  minZoom: 2,
+  minZoom: 12,
   maxZoom: 20,
   center: [138.388768, 34.971902],
   hash: true,
